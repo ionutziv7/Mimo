99d85fe146cbbe2665fa27540b966e5ef22caee8
Ivanciu Ionut <93291968+ionutziv7@users.noreply.github.com>
2022-04-06 12:18:31 +0300

    Dynamic Webpages Project Pizza Delivery

---
+++ Projects/Pizza Delivery/script.js
@@ -0,0 +1,20 @@
+var orderList = document.getElementById("orders");
+
+var marinara = document.createElement("li");
+marinara.innerHTML = "1 Pizza Marinara";
+orderList.appendChild(marinara);
+
+var romana = document.createElement("li");
+romana.innerHTML = "1 Pizza Romana, extra anchovies";
+orderList.appendChild(romana);
+
+var readyList = document.getElementById("ready");
+var margherita = document.getElementById("margherita");
+orderList.removeChild(margherita);
+readyList.appendChild(margherita);
+
+var carbonara = document.createElement("li");
+carbonara.innerHTML = "1 Pizza Carbonara";
+orderList.appendChild(carbonara);
+orderList.removeChild(carbonara);
+readyList.appendChild(carbonara);
