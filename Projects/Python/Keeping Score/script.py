1e1191f589861d306e436a5d66f9d80ba5370fa1
Ivanciu Ionut <93291968+ionutziv7@users.noreply.github.com>
2022-04-06 17:49:57 +0300

    Update script.py

--- Projects/Python/Keeping Score/script.py
+++ Projects/Python/Keeping Score/script.py
@@ -1,5 +1,5 @@
-player_1 = "Sam"
-player_2 = "Alex"
+player_1 = "John"
+player_2 = "Johnny"
 current_round = 1
 
 print("Game on!")
@@ -12,8 +12,15 @@ player_1_score = 10
 player_2_score = 13
 print(f"{player_2} wins with {player_2_score} to {player_1_score}")
 print("-------------------------")
-
 print(f"Round {current_round + 1}!")
 player_1_score = 20
-player_2_score = 5
+player_2_score = 6
 print(f"{player_1} defeats {player_2} by {player_1_score - player_2_score} points")
+total_score = player_1_score + player_2_score
+print(f"Total score of the round for players: {player_1} and {player_2} is: {total_score}")
+print(f"Round {current_round + 2}!")
+player_1_score = 75
+player_2_score = 51
+print(f"{player_1} defeats {player_2} by {player_1_score - player_2_score} points")
+total_score = player_1_score + player_2_score
+print(f"Total score of the round for players: {player_1} and {player_2} is: {total_score}")
