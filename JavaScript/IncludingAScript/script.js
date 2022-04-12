c372bc0e475c6142fa1f20467e6511dd1261aef0
Ivanciu Ionut <93291968+ionutziv7@users.noreply.github.com>
2022-04-12 16:08:54 +0300

    Html &JavaScript functions with some buttons and p
    Functions in js with some buttons tags with onclick attribute set on html document and p tags 
    with id's set. When a user clicks some buttons the function set to that specific button will create a 
    message to be shown on a screen.

---
+++ JavaScript/IncludingAScript/script.js
@@ -0,0 +1,25 @@
+function hiGuys() {
+    document.getElementById("shmee").innerHTML = "Subscribed to Shmee150";
+}
+
+//anonymous function
+var greetings = function() {
+  document.getElementById("hiFriends").innerHTML = "You know you are in good company if the website says 'Hi Friends'...";
+    console.log("Greetings!");
+}
+
+function websitePub() {
+    document.getElementById("prompt").innerHTML = "Website published!";  
+}
+
+function orderItem() {
+    document.getElementById("orderItem").innerHTML = "Item ordered!";
+}
+
+function deleteAccount() {
+    document.getElementById("deleteAccount").innerHTML = "Account Deleted!"
+}
+
+function save() {
+    document.getElementById("save").innerHTML = "Progress saved!";
+}
