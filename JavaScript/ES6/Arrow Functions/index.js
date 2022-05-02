bf4f4fe41f2b8e9245507acad7e597aadc763901
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-05-02 14:47:45 +0300

    Arrow Functions

---
+++ JavaScript/ES6/Arrow Functions/index.js
@@ -0,0 +1,38 @@
+function getGreeting() {
+    return 'Hello';
+};
+
+var greeting = () => 'Hi';
+
+console.log(getGreeting());
+console.log(greeting());
+
+var greetings = () => {
+    return 'Hello there!';
+}
+console.log(greetings);
+
+function getGreet(firstname) {
+    return ' Greetings ' + firstname;
+}
+
+var greets = (firstNamee) => {
+    return 'Greets ' + firstNamee;
+}
+console.log('John');
+
+const gretingss = () => {
+    const name = 'Elizabeth';
+    return 'Greetings ' + name;
+}
+
+const greetin = () => 'Greet';
+
+const getVehicleCount = () => {
+    count = 4003;
+    return count;
+}
+console.log(getVehicleCount());
+
+const getVehiclesCount = () => 400;
+console.log(getVehiclesCount);
