0d404488a81bcfe6974128a6882cc29218f5012c
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-05-05 15:35:04 +0300

    Two Style Sheets

---
+++ JavaScript/Querying Webpages/TwoStyleSheets/script.js
@@ -0,0 +1,9 @@
+function setRowView() {
+    var el = document.querySelector("link");
+    el.href = "style1.css";
+}
+
+function setColumnView() {
+    var el = document.querySelector("link");
+    el.href = "style2.css";
+}
