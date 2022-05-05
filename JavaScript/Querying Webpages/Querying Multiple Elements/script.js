68af02a5f75ab22780228c8ef638a3bf7905fc52
Ivanciu Ionut <ionutziv7@protonmail.com>
2022-05-05 13:00:29 +0300

    Querying Multiple Elements

---
+++ JavaScript/Querying Webpages/Querying Multiple Elements/script.js
@@ -0,0 +1,9 @@
+function displayItem() {
+    var el = document.querySelectorAll(".movie, .tech, button");
+    console.log(el[2].innerHTML);
+}
+
+function displayItems() {
+    var el = document.querySelectorAll(".urgent");
+    console.log(el.length);
+}
